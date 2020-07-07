af4fdb40ecbb22234df97a80bcd9baea847ddbc2
Kartiktharad <54314698+Kartiktharad@users.noreply.github.com>
2020-07-08 02:22:58 +0530

    improved auto bar fill in skill section

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function scrollVertically(targetSection) {
 }
 
 
-var progressBars = document.querySelectorAll(".skill-progress > div");
+/*var progressBars = document.querySelectorAll(".skill-progress > div");
 var skillsContainer = document.getElementById('skills-container');
 var animationDone = false;
 
@@ -77,7 +77,64 @@ function checkScroll() {
 
 
 
+window.addEventListener("scroll", checkScroll);*/
+
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+
+
+
+function initialiseBar(bar) {
+    bar.setAttribute("data-visited", false);
+    bar.style.width = 0 + '%';
+}
+
+for (var bar of progressBars) {
+    initialiseBar(bar);
+}
+
+
+
+function fillBar(bar) {
+
+    var currentWidth = 0;
+    var targetWidth = bar.getAttribute("data-bar-width");
+    var interval = setInterval(function () {
+        if (currentWidth >= targetWidth) {
+            clearInterval(interval);
+            return;
+        }
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    }, 5);
+
+}
+
+
+
+// This function uses a for loop for individual progress bars.
+function checkScroll() {
+
+    for (let bar of progressBars) {
+        var barCoordinates = bar.getBoundingClientRect();
+        if ((bar.getAttribute("data-visited") == "false") &&
+            (barCoordinates.top <= (window.innerHeight - barCoordinates.height))) {
+            bar.setAttribute("data-visited", true);
+            fillBar(bar);
+        } else if (barCoordinates.top > window.innerHeight) {
+            bar.setAttribute("data-visited", false);
+            initialiseBar(bar);
+        }
+
+    }
+}
+
+
+
 window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
 
 
 
